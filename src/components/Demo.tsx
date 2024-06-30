3a9aad4bc45ca6e0cb2f902c0663d6c1dbba0da5
dhruvagrawat <dhruvagrawat9@gmail.com>
2024-07-01 02:01:05 +0530

    Error Fixed

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,4 +1,3 @@
-'use client';
 import React from "react";
 import { Container } from "@/components/Container";
 import {
@@ -28,8 +27,6 @@ export const Demo = () => {
           contentArrowStyle={{
             display: "none",
             color: "transparent",
-
-
           }}
           icon={<FaCircle />} // Remove icon if not needed
         >
@@ -49,159 +46,7 @@ export const Demo = () => {
             <div className="w-full md:w-1/2 text-right">
               <img
                 src="../img/expvid/atc.gif" // Example path to GIF
-                alt="GIF"
-                className="max-w-full max-h-full rounded-lg"
-              />
-            </div>
-          </div>
-        </VerticalTimelineElement>
-
-        {/* Experience 2 */}
-        <VerticalTimelineElement
-          contentStyle={{
-            background: "transparent",
-            boxShadow: "none",
-            textAlign: "left",
-            padding: "0rem 1rem md:0rem 2rem",
-          }}
-          iconStyle={{
-            background: "rgba(255, 255, 255, 1)",
-            color: "rgb(57, 117, 234)",
-            fontSize: "4rem",
-          }}
-          icon={<FaCircle />} // Remove icon if not needed
-        >
-          <div className="flex flex-col md:flex-row">
-            <div className="w-full md:w-1/2 md:mr-4 mb-4 mr-10 md:mb-0" style={{ alignSelf: "flex-start" }}>
-              <h2 className="font-semibold capitalize">Pin the extension</h2>
-              <p className="!mt-1 !font-normal text-gray-700 ">
-                1. On the top right corner of the browser click on extensions.
-              </p>
-              <p className="!mt-1 !font-normal text-gray-700 ">
-                2. Pin JOBSquad extension to the top by clicking on pin.
-              </p>
-            </div>
-            <div className="w-full md:w-1/2 text-right">
-              <img
-                src="../img/expvid/pe.gif" // Example path to GIF
-                alt="GIF"
-                className="max-w-full max-h-full rounded-lg"
-              />
-            </div>
-          </div>
-        </VerticalTimelineElement>
-
-        {/* Experience 3 */}
-        <VerticalTimelineElement
-          contentStyle={{
-            background: "transparent",
-            boxShadow: "none",
-            textAlign: "left",
-            padding: "0rem 1rem md:0rem 2rem",
-          }}
-          iconStyle={{
-            background: "rgba(255, 255, 255, 1)",
-            color: "rgb(57, 117, 234)",
-            fontSize: "4rem",
-          }}
-          icon={<FaCircle />} // Remove icon if not needed
-        >
-          <div className="flex flex-col md:flex-row">
-            <div className="w-full md:w-1/2 md:mr-4 mb-4 mr-10 md:mb-0" style={{ alignSelf: "flex-start" }}>
-              <h2 className="font-semibold capitalize">Sign in with LinkedIn</h2>
-              <p className="!mt-1 !font-normal text-gray-700 ">
-                1. On the top right corner of the browser click on extensions.
-              </p>
-              <p className="!mt-1 !font-normal text-gray-700 ">
-                2. Click on the Job Squad Extension
-              </p>
-              <p className="!mt-1 !font-normal text-gray-700 ">
-                3. Click on Open Dashboard.
-              </p>
-              <p className="!mt-1 !font-normal text-gray-700 ">
-                4. Click on Login with LinkedIn.
-              </p>
-            </div>
-            <div className="w-full md:w-1/2 text-right">
-              <img
-                src="../img/expvid/l.gif" // Example path to GIF
-                alt="GIF"
-                className="max-w-full max-h-full rounded-lg"
-              />
-            </div>
-          </div>
-        </VerticalTimelineElement>
-
-        {/* Experience 4 */}
-        <VerticalTimelineElement
-          contentStyle={{
-            background: "transparent",
-            boxShadow: "none",
-            textAlign: "left",
-            padding: "0rem 1rem md:0rem 2rem",
-          }}
-          iconStyle={{
-            background: "rgba(255, 255, 255, 1)",
-            color: "rgb(57, 117, 234)",
-            fontSize: "4rem",
-          }}
-          icon={<FaCircle />} // Remove icon if not needed
-        >
-          <div className="flex flex-col md:flex-row">
-            <div className="w-full md:w-1/2 md:mr-4 mb-4 mr-10 md:mb-0" style={{ alignSelf: "flex-start" }}>
-              <h2 className="font-semibold capitalize">Create your Application</h2>
-              <p className="!mt-1 !font-normal text-gray-700 ">
-                1. Start your Job Application Process by clicking on CREATE APPLICATION on the menu on the left.
-              </p>
-              <p className="!mt-1 !font-normal text-gray-700 ">
-                2. Fill the Job Application form, Upload your resume, and click on Submit button on the bottom.
-              </p>
-              <p className="!mt-1 !font-normal text-gray-700 ">
-                3. Click on Close button.
-              </p>
-            </div>
-            <div className="w-full md:w-1/2 text-right">
-              <img
-                src="../img/expvid/ca.gif" // Example path to GIF
-                alt="GIF"
-                className="max-w-full max-h-full rounded-lg"
-              />
-            </div>
-          </div>
-        </VerticalTimelineElement>
-
-        {/* Experience 5 */}
-        <VerticalTimelineElement
-          contentStyle={{
-            background: "transparent",
-            boxShadow: "none",
-            textAlign: "left",
-            padding: "0rem 1rem md:0rem 2rem",
-          }}
-          iconStyle={{
-            background: "rgba(255, 255, 255, 0.9)",
-            color: "rgb(57, 117, 234)",
-            fontSize: "4rem",
-          }}
-          icon={<FaCircle />} // Remove icon if not needed
-        >
-          <div className="flex flex-col md:flex-row">
-            <div className="w-full md:w-1/2 md:mr-4 mb-4 mr-10 md:mb-0" style={{ alignSelf: "flex-start" }}>
-              <h2 className="font-semibold capitalize">Start Applying</h2>
-              <p className="!mt-1 !font-normal text-gray-700 ">
-                1. On the Start Applying page click on SELECT button under your card to select it.
-              </p>
-              <p className="!mt-1 !font-normal text-gray-700 ">
-                2. Click Close to close the pop up after your card is selected.
-              </p>
-              <p className="!mt-1 !font-normal text-gray-700 ">
-                3. Start Applying by clicking on START APPLYING WITH APPLICATION ID,
-              </p>
-            </div>
-            <div className="w-full md:w-1/2 text-right">
-              <img
-                src="../img/expvid/sa.gif" // Example path to GIF
-                alt="GIF"
+                alt="Adding extension to Chrome"
                 className="max-w-full max-h-full rounded-lg"
               />
             </div>
@@ -209,6 +54,7 @@ export const Demo = () => {
         </VerticalTimelineElement>
 
         {/* Add more VerticalTimelineElements for additional experiences */}
+        
       </VerticalTimeline>
     </Container>
   );
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -10,17 +10,15 @@ import userThreeImg from "../../public/img/user3.jpg";
 const Testimonials = () => {
   return (
     <Container>
-
       <div className="grid1">
         {/* Testimonial 1 */}
         <div className="grid2">
-          
           <div className="flex pt-10 justify-center mb-4">
-            <Image src={userOneImg} alt="User One" className="rounded-full" width={80} height={80} />
+            <Image src={userOneImg} alt="Portrait of John Doe" className="rounded-full" width={80} height={80} />
           </div>
           <div className="p-6 text-gray-300 pb-10 relative z-10 text-center">
-            <p className="text-lg mb-4">"Testimonial 1 tdgfdg dgdfgdf dfgfdgd gdfg dfg dg dg here."</p>
-            <div className="border-b-2  border-gray-300 w-4/5 mb-2 mx-auto"></div>
+            <p className="text-lg mb-4">"Testimonial 1 content here."</p>
+            <div className="border-b-2 border-gray-300 w-4/5 mb-2 mx-auto"></div>
             <p className="font-semibold text-gray-400">John Doe</p>
           </div>
         </div>
@@ -28,23 +26,23 @@ const Testimonials = () => {
         {/* Testimonial 2 */}
         <div className="grid2">
           <div className="flex pt-10 justify-center mb-4">
-            <Image src={userTwoImg} alt="User Two" className="rounded-full" width={80} height={80} />
+            <Image src={userTwoImg} alt="Portrait of Jane Smith" className="rounded-full" width={80} height={80} />
           </div>
           <div className="p-6 text-gray-300 pb-10 relative z-10 text-center">
-            <p className="text-lg mb-4">"Testimonial 2 gffdfdfdgdg dfgdfgdf dfgfdgdf fdgdgdf fdgdfghere."</p>
+            <p className="text-lg mb-4">"Testimonial 2 content here."</p>
             <div className="border-b-2 border-gray-300 w-4/5 mb-2 mx-auto"></div>
             <p className="font-semibold text-gray-400">Jane Smith</p>
           </div>
         </div>
 
         {/* Testimonial 3 */}
-        <div className="grid2" >
+        <div className="grid2">
           <div className="flex pt-10 justify-center mb-4">
-            <Image src={userThreeImg} alt="User Three" className="rounded-full" width={80} height={80} />
+            <Image src={userThreeImg} alt="Portrait of Mike Johnson" className="rounded-full" width={80} height={80} />
           </div>
           <div className="p-6 text-gray-300 pb-10 relative z-10 text-center">
-            <p className="text-lg mb-4">"Testimonfffffial fgdfg dfgdfgd dfgdfgd dgfdgd drfgdg  3 content here."</p>
-            <div className="border-b-2  border-gray-300 w-4/5 mb-2 mx-auto"></div>
+            <p className="text-lg mb-4">"Testimonial 3 content here."</p>
+            <div className="border-b-2 border-gray-300 w-4/5 mb-2 mx-auto"></div>
             <p className="font-semibold text-gray-400">Mike Johnson</p>
           </div>
         </div>
