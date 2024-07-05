87554ea3b5d4c9ea16ae5f1ff4c6929924b13a89
dhruvagrawat <dhruvagrawat9@gmail.com>
2024-07-06 00:47:37 +0530

    New Container

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,14 +3,17 @@ import { Aboutimgsection } from "@/components/About/Aboutimgsection";
 import { Mid } from "@/components/About/Mid";
 import { Mission } from "@/components/About/Mission";
 import {Last} from "@/components/About/Last";
+import {Container} from "@/components/Container";
 
 const About = () => {
   return (
     <div>
-      <Aboutimgsection />
-      <Mid />
-      <Mission />
-      <Last/>
+      
+        <Aboutimgsection />
+        <Mid />
+        <Mission />
+        <Last/>
+      
     </div>
   );
 };
--- src/components/About/Aboutimgsection.tsx
+++ src/components/About/Aboutimgsection.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import Image from "next/image";
 import imgab from "../../../public/img/about/imgab.png";
+import { Container } from "../Container";
 
 export const Aboutimgsection = () => {
   return (
     <div className="abg">
-      <div className="relative flex flex-col lg:flex-row w-full h-200 p-8 lg:p-20">
-        <div className="flex-1 text-left lg:pr-10 lg:pt-10 lg:pl-10">
-          <h1 className="text-4xl text-white pt-10 pb-5 font-bold lg:pt-20 lg:pb-5 lg:text-6xl">ABOUT US</h1>
-          <p className="max-w-xl text-left text-md pb-10 text-white">
-            Welcome to JOBSquad, your ultimate partner in the job search journey. We are a team of passionate innovators dedicated to transforming the way professionals find and apply for jobs on LinkedIn.
-            <br /><br />
-            Our mission is simple: to empower job seekers by leveraging advanced technology to simplify and enhance the job application process.
-          </p>
+      <Container>
+        <div className="relative flex flex-col lg:flex-row w-full h-200 p-8 lg:p-20">
+          <div className="flex-1 text-left lg:pr-10 lg:pt-10 lg:pl-10">
+            <h1 className="text-4xl text-white pt-10 pb-5 font-bold lg:pt-20 lg:pb-5 lg:text-6xl">ABOUT US</h1>
+            <p className="max-w-xl text-left text-md pb-10 text-white">
+              Welcome to JOBSquad, your ultimate partner in the job search journey. We are a team of passionate innovators dedicated to transforming the way professionals find and apply for jobs on LinkedIn.
+              <br /><br />
+              Our mission is simple: to empower job seekers by leveraging advanced technology to simplify and enhance the job application process.
+            </p>
+          </div>
+          <div className="lg:flex-1 flex justify-end items-center ">
+            <Image alt="abtimg" src={imgab} className="abtimg" />
+          </div>
         </div>
-        <div className="lg:flex-1 flex justify-end items-center ">
-          <Image alt="abtimg" src={imgab} className="abtimg" />
-        </div>
-      </div>
+      </Container>
     </div>
 
 
