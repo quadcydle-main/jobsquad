e0c4f1823c32155762003e981327e063e30c1efc
dhruvagrawat <dhruvagrawat9@gmail.com>
2024-07-08 12:08:18 +0530

    NEw font changes

--- src/components/About/Last.tsx
+++ src/components/About/Last.tsx
@@ -17,30 +17,30 @@ export const Last = () => {
                     <div className="flex flex-col space-y-6">
                         <div className="flex items-center lg:pb-5 space-x-4">
                             <Image src={icon1} alt="Efficiency icon" className="w-12 h-12 lg:w-16 lg:h-16" />
-                            <p className="lg:text-lg">
+                            <p className=" text-base lg:text-lg">
                                 <b>Efficiency:</b> Our extension automates the application process, saving you valuable time and effort.
                             </p>
                         </div>
                         <div className="flex items-center lg:pb-5 space-x-4">
                             <Image src={icon2} alt="Accuracy icon" className="w-12 h-12 lg:w-16 lg:h-16" />
-                            <p className="lg:text-lg">
+                            <p className=" text-base  lg:text-lg">
                                 <b>Accuracy:</b> Jobsquad applies to positions that best match your profile, increasing your chances of landing the right job.
                             </p>
                         </div>
                         <div className="flex items-center lg:pb-5 space-x-4">
                             <Image src={icon3} alt="User-Friendly icon" className="w-12 h-12 lg:w-16 lg:h-16" />
-                            <p className="lg:text-lg">
+                            <p className=" text-base  lg:text-lg">
                                 <b>User-Friendly:</b> Our intuitive interface is easy to install and use, making job applications a breeze.
                             </p>
                         </div>
                         <div className="flex items-center lg:pb-5 space-x-4">
                             <Image src={icon4} alt="Support icon" className="w-12 h-12 lg:w-16 lg:h-16" />
-                            <p className="p-1 lg:text-lg">
+                            <p className="text-base  p-1 lg:text-lg">
                                 <b>Support:</b> We are committed to supporting our users with exceptional customer service and continuous improvements to our platform.
                             </p>
                         </div>
                     </div>
-                    <p className="mt-6 text-center text-lg">
+                    <p className="mt-6 text-base text-center lg:text-lg">
                         Join the Jobsquad community today and take the first step towards a more efficient and successful job search. Let us handle the applications while you focus on achieving your career goals.
                     </p>
                 </div>
